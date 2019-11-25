3bcaa181e8ebe473e4c799e75cf741dd7686bcf7
Nasser Atiyeh <nasseratiyeh@gmail.com>
2019-11-25 17:25:22 -0600

    more work on usestage hooks

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -5,8 +5,28 @@ export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
 
   useEffect(() => {
-    const updateStage
-  }, [])
+    const updateStage = prevStage => {
+      //first flush the stage
+      const newStage = prevStage.map(row =>
+        row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
+      );
+      //then draw the tetromino
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if (value !== 0) {
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              `${player.collided ? 'merged' : 'clear'}`,
+            ]
+          }
+        });
+
+      });
+      return newStage;
+    };
+
+    setStage(prev => updateStage(prev))
+  }, [player.collided, player.pos.x, player.pos.y, player.tetromino])
 
   return [stage, setStage];
 }
