eaaf29226a1e2e320ce2d9f13ea66674c5449917
Nasser Atiyeh <nasseratiyeh@gmail.com>
2019-11-26 10:54:51 -0600

    created collision and stop at borders

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -5,3 +5,25 @@ export const createStage = () =>
   Array.from(Array(STAGE_HEIGHT), () =>
     new Array(STAGE_WIDTH).fill([0, 'clear'])
   )
+
+export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
+  for (let y = 0; y < player.tetromino.length; y += 1) {
+    for (let x = 0; x < player.tetromino[y].length; x += 1) {
+
+      //1. check that we are on a tetromino cell
+      if (player.tetromino[y][x] !== 0) {
+        if (
+        //2. check that our move is inside the game area height and bottom of play area y
+          !stage[y + player.pos.y + moveY] ||
+
+        //3. check that our move is inside the game area width x
+          !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+        //4. check that the cell we are moving through isnt set to clear
+          stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !== 'clear'
+        ) {
+          return true
+        }
+      }
+    }
+  }
+}
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -20,13 +20,17 @@ export const useStage = (player, resetPlayer) => {
             ]
           }
         });
-
       });
+      // then check if we collided
+      if (player.collided) {
+        resetPlayer();
+      }
+
       return newStage;
     };
 
     setStage(prev => updateStage(prev))
-  }, [player])
+  }, [player, resetPlayer])
 
   return [stage, setStage];
 }
